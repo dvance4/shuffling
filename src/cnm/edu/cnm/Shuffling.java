e111e4ed6bfeb2887c5800a00445b96b70129738
Dvance4 <dvance4@cnm.edu>
2019-05-28 11:10:27 -0600

    Initial commit

---
+++ src/cnm/edu/cnm/Shuffling.java
@@ -0,0 +1,114 @@
+package cnm.edu.cnm;
+
+import java.util.Random;
+
+public class Shuffling {
+
+  private static Random defaultRng;
+
+  public static void shuffle(int[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(int[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      int temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(byte[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(byte[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      byte temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(short[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(short[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      short temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(char[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(char[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      char temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(long[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(long[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      long temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(float[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(float[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      float temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  public static void shuffle(double[] data) {
+    Random rng = getRandom();
+    shuffle(data, rng);
+  }
+
+  public static void shuffle(double[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      double temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+  }
+
+  protected static Random getRandom() {
+    if (defaultRng == null) {
+      defaultRng = new Random();
+    }
+    return defaultRng;
+  }
+
+}
---
+++ test/cnm/edu/cnm/ShufflingTest.java
@@ -0,0 +1,49 @@
+package cnm.edu.cnm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+class ShufflingTest {
+
+  @Test
+  void shuffleCheckData() {
+    int[] expected = new int[1000];
+    for (int i = 0; i < expected.length; i++) {
+      expected[i] = i;
+    }
+    int[] actual = Arrays.copyOf(expected, expected.length);
+    Shuffling.shuffle(actual);
+    assertFalse(Arrays.equals(expected, actual), "Highly unlikely");
+    Arrays.sort(actual);
+    assertArrayEquals(expected, actual);
+  }
+
+  /**
+   * Assume a Fisher-Yates shuffle that iterates from last to first element, and uses
+   * {@link java.util.Random#nextInt(int)}.
+   */
+  @Test
+  void shuffleDegenerateRng() {
+    Random rng = new DegenerateRandom();
+    int[] expected = new int[1000];
+    for (int i = 0; i < expected.length; i++) {
+      expected[i] = i;
+    }
+    int[] actual = Arrays.copyOf(expected, expected.length);
+    Shuffling.shuffle(actual, rng);
+
+  }
+
+  private class DegenerateRandom extends Random {
+
+    @Override
+    public int nextInt(int bound) {
+      return bound - 1;
+    }
+
+  }
+
+}
